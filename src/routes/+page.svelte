c7ba9b112df7bd3ca55ef727779a19f18776eabf
Aidan <aidanmarler1@gmail.com>
2025-12-08 16:29:47 -0700

    Changed page message and added temp style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="font-bold mt-20 text-4xl w-full text-center">Welcome to marler.cc</h1>
+
+<p class="mt-2 text-xl w-full text-center">I bought this domain to create my personal website - expect it up by Februrary.</p>
+
